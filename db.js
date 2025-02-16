9caa3dd7af46b07489e7cb2f6b86c9f6067f8450
Samuel Xavier <samueloliveira.xavier@hotmail.com>
2025-02-16 06:59:40 -0300

    Menu Backend - Versão inicial

---
+++ db.js
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import postgres from 'postgres'
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const URL = `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`;
+
+export const sql = postgres(URL, { ssl: 'require' });
+
